1abcef080605dfb16f3d4e90cbd56c8b96114fca
xTheDawg <sascha.ledermann@bluewin.ch>
2019-04-25 22:05:06 +0200

    Beginn der Version 1

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,19 @@
+import tetris.gui.Block;
+import tetris.gui.GUI;
+
 public class Game {
+    private GUI gui;
+
+    public Game(GUI gui) {
+        this.gui = gui;
+    }
+
+
+    public void start() {
+        Block block = new Block(5, 5, 3);
+        gui.drawBlock(block);
+        while(true) {
+
+        }
+    }
 }
---
+++ src/Tetris.java
@@ -0,0 +1,9 @@
+import tetris.gui.GUI;
+
+public class Tetris {
+    public static void main(String[] args) {
+        GUI gui = new GUI(10, 20);
+        Game game = new Game(gui);
+        game.start();
+    }
+}
